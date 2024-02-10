f608af93a4eef537d1da7210dad4651a8faca19a
StpehenChinag <stephenchinag@gmail.com>
2024-02-10 15:28:30 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,2 +1,15 @@
+import { Link } from "react-router-dom";
 
+const Header = () => {
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/"> Mike Hotel Project </Link>
+        </span>
+      </div>
+    </div>
+  );
+};
 
+export default Header;
