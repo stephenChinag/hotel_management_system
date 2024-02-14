bac337679524160808162315ba5ee06dba4d13dd
StpehenChinag <stephenchinag@gmail.com>
2024-02-14 09:02:41 +0100

    env

--- src/api-client.ts
+++ src/api-client.ts
@@ -4,7 +4,8 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(
-    "http://13.48.114.201/api/registerandupdate_user/",
+    `${API_BASE_URL}api/registerandupdate_user/`,
+    // "http://13.48.114.201/api/registerandupdate_user/",
     {
       method: "POST",
       credentials: "include",
