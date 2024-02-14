0def2ed774c19817cb0cf66854067a1c214b8436
StpehenChinag <stephenchinag@gmail.com>
2024-02-14 02:38:24 +0100

    api client

--- src/api-client.ts
+++ src/api-client.ts
@@ -3,13 +3,16 @@ import { RegisterFormData } from "./pages/Register";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const register = async (formData: RegisterFormData) => {
-  const response = await fetch(`${API_BASE_URL}/api/registerandupdate_user`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  });
+  const response = await fetch(
+    "http://13.48.114.201/api/registerandupdate_user/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }
+  );
 
   const responseBody = await response.json();
 
